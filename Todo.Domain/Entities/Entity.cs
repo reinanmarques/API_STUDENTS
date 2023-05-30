d8612c4bc8117363665453638add1829656297b0
reinanmarques <wesleremarques@gmail.com>
2023-05-30 16:49:53 -0300

    first commit

---
+++ Todo.Domain/Entities/Entity.cs
@@ -0,0 +1,17 @@
+
+namespace Todo.Domain.Entities;
+
+public  abstract class Entity : IEquatable<Entity>
+{
+    public Guid Id { get; private set; }
+
+    public Entity()
+    {
+        Id = Guid.NewGuid();
+    }
+
+    public bool Equals(Entity? other)
+    {
+        return other != null && Id == other.Id;
+    }
+}
---
+++ Todo.Domain/Entities/Student.cs
@@ -0,0 +1,7 @@
+namespace Todo.Domain.Entities;
+
+public class Student : Entity
+{
+    public string name { get; set; }
+    public string course { get; set; }
+}
